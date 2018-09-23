ad247c0b37b64795ff3eac9f220041ebcfb0bef4
Prince Billy Graham Karmoker <princebillygrahamkarmoker@gmail.com>
2018-09-23 15:12:20 +0600

    commit

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
-#define SRC "src/test.txt";
-#define DISTCSV "src/test.txt";
-#define DISTSSQL "dist/resultsql.sql";
+#define SRC "src/test.txt"
+#define DISTCSV "dist/test.csv"
+#define DISTSSQL "dist/resultsql.sql"
 
 #include<iostream>
 #include<fstream>
-#include<cstdio>
+#include<cctype>
 using namespace std;
 
 
 int main(){
-    ifstream src("src/test.txt");
-    ofstream dist("dist/test.txt");\
+    ifstream src(SRC);
+    ofstream dist(DISTCSV);
     char r;
     int numberOfSubject;
     cout<<"Enter number of subject: ";
@@ -19,61 +19,42 @@ int main(){
     while(!src.eof()){
         if(src.get()=='|'){
             src.get();
+            for(int i=0;i<3;i++){
+                //Roll No Reg Session
+                while((r=src.get())!=' ')
+                    dist.put(r);
+                dist.put(',');
 
-            //Roll No
-            while((r=src.get())!=' ')
-                dist.put(r);
-            dist.put(',');
-
-            while((r=src.get())!='|');
-            src.get();
-
-            //Reg No.
-            while((r=src.get())!=' ')
-                dist.put(r);
-            dist.put(',');
-
-            while((r=src.get())!='|');
-            src.get();
-
-            //Session.
-            while((r=src.get())!=' ')
-                dist.put(r);
-            dist.put(',');
-
-            while((r=src.get())!='|');
-            src.get();
-
+                while((r=src.get())!='|');
+                src.get();
+            }
             //Examines
             while((r=src.get())!='|')
                 dist.put(r);
             dist.put(',');
 
-            src.get();
-
             //Grades
+            //*
             for(int i=0;i<numberOfSubject;i++){
                 //*
-                while((r=src.get())!=' ')
+                src.get();
+                src.get();
+                while((r=src.get())!=' '){
                     dist.put(r);
+                }
                 if(i!=numberOfSubject-1)
-                    dist.put(',');
+                        dist.put(',');
                 while((r=src.get())!='|');
                 //*/
-                /*
-                dist<<src;
-                src.get();src.get();dist.put(',');
-                //*/
             }
+            //*/
             dist.put('\n');
-
         }
     }
     //*/
-
     dist.close();
     src.close();
-    system("gedit dist/test.txt");
+    system("gedit dist/test.csv");
 
     return 0;
 }
