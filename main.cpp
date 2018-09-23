c2c0bab87d63025bb5f7d2178f06e904bda33feb
Prince Billy Graham Karmoker <princebillygrahamkarmoker@gmail.com>
2018-09-23 19:55:32 +0600

    commit

--- main.cpp
+++ main.cpp
@@ -18,67 +18,99 @@ int main(int argc, char *argv[]){
     cout<<"destination: "<<DIST<<endl;
     cout<<"done"<<endl;
 
-    //opening file
-    cout<<"opening file..."<<endl;
-    ifstream src(SRC);
-    //creating file
-    cout<<"creating csv file..."<<endl;
-    ofstream dist(DIST);
-    cout<<"done"<<endl;
-
-    char r;
+    //setting show cmd
+    string cmdout= "gedit \'"+DIST+"\'";
+    string cmdin= "gedit \'"+SRC+"\'";
+    //declaring important varriable
+    bool choice=true;
+    char ok,r;
     int numberOfSubject;
-    cout<<"enter number of subject: ";
-    cin>>numberOfSubject;
 
-    //Generating result
-    cout<<"generating file..."<<endl;
-    //*
-    while(!src.eof()){
-        if(src.get()=='|'){
-            src.get();
-            for(int i=0;i<3;i++){
-                //Roll No Reg Session
-                while((r=src.get())!=' ')
-                    dist.put(r);
-                dist.put(',');
+    //attempting
+    for(int count=1;choice;count++){
+        cout<<"Attempt "<<count<<":"<<endl;
+        system(cmdin.c_str());
 
-                while((r=src.get())!='|');
-                src.get();
-            }
-            //Examines
-            while((r=src.get())!='|')
-                dist.put(r);
-            dist.put(',');
 
-            //Grades
+        //opening file
+        cout<<"opening file..."<<endl;
+        ifstream src(SRC);
+        //creating file
+        cout<<"creating csv file..."<<endl;
+        ofstream dist(DIST);
+        cout<<"done"<<endl;
+
+
+        if(count==1){
+            cout<<"enter number of subject: ";
+            cin>>numberOfSubject;
+        }
+
+         //Generating result
+            cout<<"generating file..."<<endl;
             //*
-            for(int i=0;i<numberOfSubject;i++){
-                //*
-                src.get();
-                src.get();
-                while((r=src.get())!=' '){
-                    dist.put(r);
-                }
-                if(i!=numberOfSubject-1)
+            while(!src.eof()){
+                if(src.get()=='|'){
+                    src.get();
+                    for(int i=0;i<3;i++){
+                        //Roll No Reg Session
+                        while((r=src.get())!=' ')
+                            dist.put(r);
                         dist.put(',');
-                while((r=src.get())!='|');
-                //*/
+
+                        while((r=src.get())!='|');
+                        src.get();
+                    }
+                    //Examines
+                    while((r=src.get())!='|')
+                        dist.put(r);
+                    dist.put(',');
+
+                    //Grades
+                    //*
+                    for(int i=0;i<numberOfSubject;i++){
+                        //*
+                        src.get();
+                        src.get();
+                        while((r=src.get())!=' '){
+                            dist.put(r);
+                        }
+                        if(i!=numberOfSubject-1)
+                                dist.put(',');
+                        while((r=src.get())!='|');
+                        //*/
+                    }
+                    //*/
+                    dist.put('\n');
+                }
             }
             //*/
-            dist.put('\n');
-        }
-    }
-    //*/
-    dist.close();
-    src.close();
-    cout<<"done"<<endl;
+            dist.close();
+            src.close();
+            cout<<"done"<<endl;
 
-    //Opening file
-    cout<<"opening result..."<<endl;
-    string cmd= "gedit "+DIST;
-    system(cmd.c_str());
-    cout<<"done"<<endl;
+            //Opening file
+            cout<<"opening result..."<<endl;
+            system(cmdout.c_str());
+            cout<<"done"<<endl;
+
+        cout<<"IS EVERY THING OK?(Y?N) ";
+        cin>>ok;
+        while(1){
+            if(ok=='Y'||ok=='y'){
+                choice=false;
+                cout<<"Attempt "<<count<<" successful see you again goodbye"<<endl;
+                break;
+            }
+            else if(ok=='N'||ok=='n'){
+                cout<<"Attempt "<<count<<" failed"<<endl;
+                break;
+            }
+            cout<<"Invalid"<<endl;
+        }
+
+
+    }
 
     return 0;
 }
